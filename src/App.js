3f0fb765410d40c9820e3fe8dd4198ad71632a24
a-toro <wtorobenavides@gmail.com>
2022-08-09 11:53:33 -0500

    Create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="main">
-      <h1>App</h1>
-      <p>Deserunt do est sit adipisicing tempor magna aute quis nulla dolore. Esse velit ad consequat ex pariatur occaecat ut ut aliquip velit proident. Culpa adipisicing commodo sit occaecat Lorem ut est ut culpa enim pariatur. Est cillum quis ad cillum fugiat dolore officia irure.</p>
+      <Header/>
     </div>
   );
 }
