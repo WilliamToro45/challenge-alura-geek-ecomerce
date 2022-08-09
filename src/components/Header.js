d3254ec83c2e01b12782db051d3fc4f362c0ffc1
a-toro <wtorobenavides@gmail.com>
2022-08-09 16:18:29 -0500

    First preview of the finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,28 @@
+import logo from '../assets/logo.svg'
+import SearchIcon from './SearchIcon';
+
 function Header() {
     return(
-        <h1>Header</h1>
+        <header className='header'>
+            <nav className='navbar'>
+                <div className='navbar__item item1'>
+                    <div className="navbar__item--logo">
+                        <img className='navbar__item--logo-icon' src={logo} alt="Logo Alura Geek"></img>
+                        <span className='navbar__item--logo-name' />
+                    </div>
+                    <div className='navbar__item--search'>
+                        <input className='navbar__item--search-input' type='search' placeholder='¿Qué desea buscar?'/>
+                        <span className='navbar__item--search-logo'><SearchIcon /></span>
+                    </div>
+                </div>
+                <div className='navbar__item item2'>
+                    <button className='navbar__item--button'>Login</button>
+                </div>
+                <div className='navbar__item hidden item3'>
+                    <SearchIcon />
+                </div>
+            </nav>
+        </header>
     )
 }
  
