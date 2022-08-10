dececb0e57cf66701dcb07a4e29d579a6390db46
a-toro <wtorobenavides@gmail.com>
2022-08-10 12:33:19 -0500

    Create ProductsGallery component

---
+++ src/components/ProductsGallery.js
@@ -0,0 +1,29 @@
+import products from "../data/data.json";
+import ProductsList from "./ProductsList";
+
+/**
+ * 
+ * @returns - Gallery for each unique type of products.
+ */
+function ProductsGallery(){
+    // Get unique categories
+    const categories = [...new Set(products.map(product => product.category))];
+    
+    return(
+        <section className="products-gallery">
+            {
+                categories.map(category => {
+                    // Get products list for each category.
+                    const productsByCategory = products.filter(product => product.category === category)
+                    return(
+                        <article className="products-galley__category" key={category}>
+                            <ProductsList category={category} productsByCategory={productsByCategory} />
+                        </article>
+                    )
+                })
+            }
+        </section>
+    )
+}
+
+export default ProductsGallery;
