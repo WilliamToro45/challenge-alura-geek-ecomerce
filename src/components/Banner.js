ad51f93564835d323289728c935ea14c3b1c7ba1
a-toro <wtorobenavides@gmail.com>
2022-08-09 19:57:29 -0500

    Create Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+function Banner(){
+    return(
+        <section className='banner'>
+            <h2 className='banner__tittle'>Febrero pormocional</h2>
+            <p className='banner__paragraph'>Productos seleccionados con 33% de descuento</p>
+            <button className='banner__button'>Ver consolas</button>
+        </section>
+    )
+}
+
+export default Banner;
