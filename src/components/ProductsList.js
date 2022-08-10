f2bc8a146640ab9dfbf2c71a4bb034e0a4914abd
a-toro <wtorobenavides@gmail.com>
2022-08-10 12:32:55 -0500

    Create ProductListComponent

---
+++ src/components/ProductsList.js
@@ -0,0 +1,32 @@
+import ProductCard from "./ProductCard";
+import { AiOutlineArrowRight } from 'react-icons/ai';
+/**
+ * 
+ * @param {string} category - category of the products. 
+ * @param {Array} productsByCategory - array with products filter by category.
+ * @returns List of products by category.
+ */
+
+function ProductsList({ category , productsByCategory }){
+    return(
+        <div className="product-list">
+            <div className="product-list__head">
+                <h2 className="product-list__head--title">{category}</h2>
+                <a href={`#${category}`} className="product-list__head--link">Ver todo <AiOutlineArrowRight/></a>
+            </div>
+            <div className="product-list__body">
+                {
+                    productsByCategory.map(product =>{
+                        return(
+                            <div className="product" key={product.id}>
+                                <ProductCard product={product}/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProductsList;
