d1a39461231a2df1d1ef365f90548b4601bbb188
a-toro <wtorobenavides@gmail.com>
2022-08-10 18:12:09 -0500

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ProductsGallery from "./components/ProductsGallery";
 
@@ -10,6 +11,7 @@ function App() {
       <Banner/>
       <ProductsGallery />
       <Contact/>
+      <Footer/>
     </div>
   );
 }
