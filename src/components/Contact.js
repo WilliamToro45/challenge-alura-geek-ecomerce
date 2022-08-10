319ea9255d1d98423d91aca7a85fb87b4afb6c17
a-toro <wtorobenavides@gmail.com>
2022-08-10 16:30:57 -0500

    Create Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,39 @@
+import LogoAluraGeek from "./LogoAluraGeek";
+
+function Contact(){
+    const handelSubmit = (event) => {
+        event.preventDefault();
+    }
+    return(
+        <section className="contact">
+            <div className="contact__section">
+                <div className="contact__logo">
+                    <LogoAluraGeek/>
+                </div>
+                <ul className="contact__list">
+                    <li className="contact__list--item">Quienes somos</li>
+                    <li className="contact__list--item">Política de privacidad</li>
+                    <li className="contact__list--item">Programa de fidelidad</li>
+                    <li className="contact__list--item">Nuestras tiendas</li>
+                    <li className="contact__list--item">Quiero ser Franquiciado</li>
+                    <li className="contact__list--item">Anúncie aquí</li>
+                </ul>
+            </div>
+            <form className="contact__form" onSubmit={handelSubmit}>
+                <div className="contact__form--head">
+                    <h2 className="contact__form--head-title">Hable con nosotros</h2>
+                </div>
+                <div className="contact__form--body">
+                    <label className="contact__form--body-label">
+                        <span>Nombre</span>
+                        <input className="contact__form--body-input" type="text" placeholder="Nombre" />
+                    </label>
+                    <textarea className="contact__form--body--textarea" placeholder="Escribe tu mensaje.."/>
+                </div>
+                <button className="contact__form--button" type="submit">Enviar mensaje</button>
+            </form>
+        </section>
+    );
+}
+
+export default Contact;
