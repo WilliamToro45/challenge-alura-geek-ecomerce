2cb4509439e8393e30da3afce7c72485d7774f1d
a-toro <wtorobenavides@gmail.com>
2022-08-10 21:44:33 -0500

    Add link to login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,41 @@
-import LogoAluraGeek from './LogoAluraGeek';
-import SearchIcon from './SearchIcon';
+import LogoAluraGeek from "./LogoAluraGeek";
+import SearchIcon from "./SearchIcon";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return(
-        <header className='header'>
-            <nav className='navbar'>
-                <div className='navbar__item item1'>
-                    <LogoAluraGeek/>
-                    <div className='navbar__item--search'>
-                        <input className='navbar__item--search-input' type='search' placeholder='¿Qué desea buscar?'/>
-                        <span className='navbar__item--search-logo'><SearchIcon /></span>
-                    </div>
-                </div>
-                <div className='navbar__item item2'>
-                    <button className='navbar__item--button'>Login</button>
-                </div>
-                <div className='navbar__item hidden item3'>
-                    <SearchIcon />
-                </div>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="navbar__item item1">
+          <LogoAluraGeek />
+          <div className="navbar__item--search">
+            <input
+              className="navbar__item--search-input"
+              type="search"
+              placeholder="¿Qué desea buscar?"
+            />
+            <span className="navbar__item--search-logo">
+              <SearchIcon />
+            </span>
+          </div>
+        </div>
+        <div className="navbar__item item2">
+            <Link
+              to="login"
+              className="navbar__item--link"
+              style={{ textDecoration: "none", color: "var(--main-color)" }}
+            >
+          <button className="navbar__item--button">
+              Login
+          </button>
+          </Link>
+        </div>
+        <div className="navbar__item hidden item3">
+          <SearchIcon />
+        </div>
+      </nav>
+    </header>
+  );
 }
- 
+
 export default Header;
