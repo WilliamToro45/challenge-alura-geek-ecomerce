c2468ab8c4b472a8a0652b9a017c762fb5d9404f
a-toro <wtorobenavides@gmail.com>
2022-08-11 13:09:02 -0500

    End AllProduct page component

--- src/pages/AllProducts.js
+++ src/pages/AllProducts.js
@@ -1,6 +1,29 @@
+import "../assets/css/all-products.css";
+import products from "../data/data.json";
+import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
+
 function AllProducts(){
     return(
-        <h1>All products</h1>
+        <section className="all-products">
+                <div className="all-products__head">
+                    <h2 className="all-products__head--title" >Todos los productos</h2>
+                    <Link to="add-product">
+                        <button className="all-products__head--button">Agregar producto</button>
+                    </Link>
+                </div>
+                <div className="all-products__body">
+                    {
+                        products.map(product => {
+                            return(
+                                <div className="all-products__body--product" key={product.id}>
+                                    <ProductCard product={product} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+        </section>
     )
 }
 
