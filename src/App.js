e0586cef50be1ce4db2b54f99cce7cd4db4bf8c4
a-toro <wtorobenavides@gmail.com>
2022-08-11 14:58:20 -0500

    Add AddProducto component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PageNotFound from "./pages/PageNotFound";
 import SharedLayout from "./pages/SharedLayout";
 import Login from "./pages/Login";
 import AllProducts from "./pages/AllProducts";
+import AddProduct from "./pages/AddProduct";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route index element={ <Home/> }/>
             <Route path="products" element={ <AllProducts/> }/>
             <Route path="login" element={ <Login/> }/>
+            <Route path="add-product" element={ <AddProduct/> }/>
           </Route>
           <Route path="*" element={ <PageNotFound/> }/>
         </Routes>
