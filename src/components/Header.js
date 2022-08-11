bdfb1543f3ad4182639b1617100c6734c4390133
a-toro <wtorobenavides@gmail.com>
2022-08-11 11:22:41 -0500

    feat: add ability to button to change dependent to current page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,30 @@
+import React from "react";
 import LogoAluraGeek from "./LogoAluraGeek";
 import SearchIcon from "./SearchIcon";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+/**
+ * 
+ * @returns - the common header of the pages.
+ */
 
 function Header() {
+  const currentPathName = useLocation().pathname;
+  const [isButtonVisible, setIsButtonVisible] = React.useState(true);
+  const [buttonTextContent, setButtonTextContent] = React.useState("Login");
+
+  React.useEffect(() => {
+    if(currentPathName.includes("login")) return setIsButtonVisible(false);
+
+    if(currentPathName.includes("admin")) {
+      setIsButtonVisible(true);
+      setButtonTextContent("Menú Administrador")
+    };
+    
+    setIsButtonVisible(true);
+
+  }, [currentPathName]);
+
   return (
     <header className="header">
       <nav className="navbar">
@@ -19,17 +41,19 @@ function Header() {
             </span>
           </div>
         </div>
-        <div className="navbar__item item2">
+        {isButtonVisible && (
+          <div className="navbar__item item2">
             <Link
               to="login"
               className="navbar__item--link"
               style={{ textDecoration: "none", color: "var(--main-color)" }}
             >
-          <button className="navbar__item--button">
-              Login
-          </button>
-          </Link>
-        </div>
+              <button className="navbar__item--button">
+                {buttonTextContent}
+              </button>
+            </Link>
+          </div>
+        )}
         <div className="navbar__item hidden item3">
           <SearchIcon />
         </div>
