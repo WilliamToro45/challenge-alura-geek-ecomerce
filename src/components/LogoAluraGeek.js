c38a35d922ddcec76a8c232bd1591dde7f22c487
a-toro <wtorobenavides@gmail.com>
2022-08-10 21:44:53 -0500

    Add link for home page

--- src/components/LogoAluraGeek.js
+++ src/components/LogoAluraGeek.js
@@ -1,12 +1,13 @@
 import '../assets/css/logo.css';
 import logo from '../assets/logo.svg'
+import { Link } from 'react-router-dom';
 
 function LogoAluraGeek(){
     return(
-        <div className="logo">
+        <Link to="/" style={{ textDecoration: "none"}} className="logo">
             <img className='logo__icon' src={logo} alt="Logo Alura Geek"></img>
             <span className='logo__name' />
-        </div>
+        </Link>
     )
 }
 
