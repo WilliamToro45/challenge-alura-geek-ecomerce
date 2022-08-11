908acbb0c14d7f299eb007bf1e4ab1d790593f3f
a-toro <wtorobenavides@gmail.com>
2022-08-10 21:09:02 -0500

    Create Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import Banner from "../components/Banner";
+import ProductsGallery from "../components/ProductsGallery";
+
+/**
+ * 
+ * @returns the home page.
+ */
+
+function Home(){
+    return(
+        <>
+            <Banner/>        
+            <ProductsGallery />
+        </>
+    );
+}
+
+export default Home;
