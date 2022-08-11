98cafbdf5aab98decff67acbc43dcd5060bd9cd6
a-toro <wtorobenavides@gmail.com>
2022-08-10 22:17:22 -0500

    End Login page component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,28 @@
+import "../assets/css/login.css";
+
+/**
+ * 
+ * @returns a page with the form of login users.
+ */
+
 function Login(){
     return ( 
-        <h1>Login</h1>
+        <section className="login">
+            <form className="login__form">
+                <h2 className="login__form--head"> Iniciar sesión</h2>
+                <input 
+                    className="login__form--input"
+                    type="text"
+                    placeholder="Escriba su correo electrónico"
+                />
+                <input 
+                    className="login__form--input"
+                    type="password"
+                    placeholder="Escriba su constraseña"
+                />
+                <button className="login__form--button">Entrar</button>
+            </form>
+        </section>
      );
 }
  
